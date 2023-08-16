80d7ce99ccfa1db3c021f681df36ed10d5efdd3f
Samson Ocran <samson.ocran@gmail.com>
2023-08-16 10:46:10 +0100

    save movie component updated

--- src/pages/saved-movies/SavedMovies.js
+++ src/pages/saved-movies/SavedMovies.js
@@ -3,21 +3,23 @@ import React from "react";
 const SavedMovies = () => {
   const storedMoviesJSON = localStorage.getItem("savedMovies");
   const storedMovies = storedMoviesJSON ? JSON.parse(storedMoviesJSON) : [];
-console.log(storedMovies)
+  console.log(storedMovies);
   return (
     <div className="px-4">
       <h1 className="text-6xl font-bold text-center">Saved Movies</h1>
       <div className="flex justify-between flex-wrap">
         {storedMovies.map((movie) => (
           <div key={movie.id}>
-            <p>{movie.title}</p>
+            <p className="font-bold text-3xl">{movie.title}</p>
             <img
-            src={`https://image.tmdb.org/t/p/w400${movie.backdrop_path}`}
-            alt={`${movie.title} poster`}
-            className="my-4"
-          />
+              src={`https://image.tmdb.org/t/p/w400${movie.backdrop_path}`}
+              alt={`${movie.title} poster`}
+              className="my-4"
+            />
+            <p>{movie.overview}</p>
+            <p className="mt-2">Popularity: {movie.popularity}</p>
+            <p className="font-bold my-2">Release year: {movie.release_date}</p>
           </div>
-          
         ))}
       </div>
     </div>
